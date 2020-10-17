9d030dd082f6fc5be89412bc88fe216aadac16dd
Scream <skate_max@mail.ru>
2020-10-17 13:54:08 +0300

    Updates

---
+++ precache-manifest.0bcd7dca315b740e71231d0e7dae5de1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10b78e7a471fb75d15ddd01960eccd29",
+    "url": "/Profile_setting_React/index.html"
+  },
+  {
+    "revision": "2e55df7564663e2a69b2",
+    "url": "/Profile_setting_React/static/css/main.539ee5eb.chunk.css"
+  },
+  {
+    "revision": "6cd3c6fe19d6151839d3",
+    "url": "/Profile_setting_React/static/js/2.ad2cb655.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/Profile_setting_React/static/js/2.ad2cb655.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e55df7564663e2a69b2",
+    "url": "/Profile_setting_React/static/js/main.84325482.chunk.js"
+  },
+  {
+    "revision": "5b20667f302f3cce8756",
+    "url": "/Profile_setting_React/static/js/runtime-main.5bf8090e.js"
+  }
+]);
